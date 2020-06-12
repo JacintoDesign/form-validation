254be2acbb55e50eea8352c4eb78487f79927a8e
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-12 14:56:21 -0400

    final production code

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
-const form = document.getElementById("signUpForm");
-const password1El = document.getElementById("password1");
-const password2El = document.getElementById("password2");
-const message = document.getElementById("message-container");
-const email = document.getElementById("email");
+const form = document.getElementById('signUpForm');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
 
-form.addEventListener("submit", function (e) {
-  // Prevent form from submitting
+function storeFormData(e) {
+  const user = {
+    name: e.srcElement[0].value,
+    phone: e.srcElement[1].value,
+    email: e.srcElement[2].value,
+    website: e.srcElement[3].value,
+    password: e.srcElement[4].value,
+  };
+  // Do something with user data
+  console.log(user);
+}
+
+function validateForm(e) {
   e.preventDefault();
-
   // Use HTML constraint API to check form validity
-  let isValidForm = form.checkValidity();
-  //console.log(isValidForm);
-
+  const isValidForm = form.checkValidity();
   // If the form isn't valid
-  if (isValidForm === false) {
+  if (!isValidForm) {
     // Style main message for an error
-    message.innerHTML = "<h3>Please fill out all fields.</h3>";
-    message.style.color = "red";
-    message.style.borderColor = "red";
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
   }
-
-  // console.log(password1El.value);
   // Check to see if both password fields match
   let passwordsMatch = false;
   if (password1El.value === password2El.value) {
     // If they match, set the border color of input to green
     passwordsMatch = true;
-    password1El.style.borderColor = "green";
-    password2El.style.borderColor = "green";
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
   }
-  if (
-    password1El.value &&
-    password2El.value &&
-    password1El.value !== password2El.value
-  ) {
+  if (password1El.value && password2El.value && password1El.value !== password2El.value) {
     // If they don't match, border color of input to red, change message
     passwordsMatch = false;
-    message.style.color = "red";
-    message.style.borderColor = "red";
-    password1El.style.borderColor = "red";
-    password2El.style.borderColor = "red";
-    message.innerHTML = "<h3>Make sure passwords match.</h3>";
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+    message.textContent = 'Make sure passwords match.';
   }
-
   // If form is valid and passwords match
-  if (
-    isValidForm === true &&
-    password1El.value &&
-    password2El.value &&
-    passwordsMatch === true
-  ) {
+  if (isValidForm && passwordsMatch) {
     // Style main message for success
-    message.innerHTML = "<h3>Successfully Registered!</h3>";
-    message.style.color = "green";
-    message.style.borderColor = "green";
+    message.textContent = 'Successfully Registered!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+    // Store User Data
+    storeFormData(e);
   }
-});
+}
+
+// Event Listener
+form.addEventListener('submit', validateForm);
