f532201a78b37eed7ed427398ecc52202086cbc5
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-13 19:50:43 -0400

    final production code

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
-function storeFormData() {
-  const user = {
-    name: form.name.value,
-    phone: form.phone.value,
-    email: form.email.value,
-    website: form.website.value,
-    password: form.password.value,
-  };
-  // Do something with user data
-  console.log(user);
-}
+let isValidForm = false;
 
-function validateForm(e) {
-  e.preventDefault();
+function validateForm() {
   // Use HTML constraint API to check form validity
-  const isValidForm = form.checkValidity();
+  isValidForm = form.checkValidity();
   // If the form isn't valid
   if (!isValidForm) {
     // Style main message for an error
@@ -50,10 +39,30 @@ function validateForm(e) {
     message.textContent = 'Successfully Registered!';
     message.style.color = 'green';
     messageContainer.style.borderColor = 'green';
-    // Store User Data
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  // Validate Form
+  validateForm();
+  // Submit Form if Valid
+  if (isValidForm) {
     storeFormData();
   }
 }
 
 // Event Listener
-form.addEventListener('submit', validateForm);
+form.addEventListener('submit', processFormData);
