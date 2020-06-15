210ea3f3a75449532d859c1e92aaf9433f7e5269
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-15 15:54:43 -0400

    final production code

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const form = document.getElementById('signUpForm');
+const form = document.getElementById('form');
 const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
-let isValidForm = false;
+let isValid = false;
 
 function validateForm() {
   // Use HTML constraint API to check form validity
-  isValidForm = form.checkValidity();
+  isValid = form.checkValidity();
   // If the form isn't valid
-  if (!isValidForm) {
+  if (!isValid) {
     // Style main message for an error
     message.textContent = 'Please fill out all fields.';
     message.style.color = 'red';
@@ -20,21 +20,19 @@ function validateForm() {
   // Check to see if both password fields match
   let passwordsMatch = false;
   if (password1El.value === password2El.value) {
-    // If they match, set the border color of input to green
+    // If they match, set value to true
     passwordsMatch = true;
-    password1El.style.borderColor = 'green';
-    password2El.style.borderColor = 'green';
-  } else if (password1El.value && password2El.value && password1El.value !== password2El.value) {
+  } else {
     // If they don't match, border color of input to red, change message
+    message.textContent = 'Make sure passwords match.';
     message.style.color = 'red';
     messageContainer.style.borderColor = 'red';
     password1El.style.borderColor = 'red';
     password2El.style.borderColor = 'red';
-    message.textContent = 'Make sure passwords match.';
     return;
   }
   // If form is valid and passwords match
-  if (isValidForm && passwordsMatch) {
+  if (isValid && passwordsMatch) {
     // Style main message for success
     message.textContent = 'Successfully Registered!';
     message.style.color = 'green';
@@ -59,7 +57,7 @@ function processFormData(e) {
   // Validate Form
   validateForm();
   // Submit Form if Valid
-  if (isValidForm) {
+  if (isValid) {
     storeFormData();
   }
 }
